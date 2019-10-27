4df7361080b87dd33be3c131d9501f34707e00e8
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-27 21:34:34 +0300

    explore widgets

--- sample hello.py
+++ sample hello.py
@@ -1,8 +1,24 @@
 import streamlit as st
 import math
+import pandas as pd
 x = st.slider('x')
 st.write(x, 'factorial is', math.factorial(x))
-# exloring widgets
+# exploring widgets
 # text input
 value = st.text_input('Enter Values of X')
 st.write('The Entered value is', value)
+#checkbox
+df = pd.read_csv("football_data.csv")
+if st.checkbox('Show dataframe'):
+    st.write(df)
+
+#selectionbox
+option = st.selectbox(
+    'Which Club do you like best?',
+     df['Club'].unique())
+st.write('You selected: ', option)
+
+# multi selector
+options = st.multiselect(
+ 'What are your favorite clubs?', df['Club'].unique())
+st.write('You selected:', options)
