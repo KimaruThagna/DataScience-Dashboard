2cd1b92470a05919ea8c273f4c78a65369e9830c
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-27 21:25:17 +0300

    text input widget

--- sample hello.py
+++ sample hello.py
@@ -2,3 +2,7 @@ import streamlit as st
 import math
 x = st.slider('x')
 st.write(x, 'factorial is', math.factorial(x))
+# exloring widgets
+# text input
+value = st.text_input('Enter Values of X')
+st.write('The Entered value is', value)
