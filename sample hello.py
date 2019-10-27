55ee422ec460e47e71e32eb2ff1d1ef0bd597c55
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-27 21:54:43 +0300

    create simple dashboard display

--- sample hello.py
+++ sample hello.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import math
 import pandas as pd
+from matplotlib import pyplot as plt
+
 x = st.slider('x')
 st.write(x, 'factorial is', math.factorial(x))
 # exploring widgets
@@ -8,17 +10,29 @@ st.write(x, 'factorial is', math.factorial(x))
 value = st.text_input('Enter Values of X')
 st.write('The Entered value is', value)
 #checkbox
-df = pd.read_csv("football_data.csv")
+df = pd.read_csv("datasets/land_under_Irrigation.csv")
 if st.checkbox('Show dataframe'):
     st.write(df)
 
 #selectionbox
 option = st.selectbox(
-    'Which Club do you like best?',
-     df['Club'].unique())
+    'Which County do you wish to View?',
+     df['County'].unique())
 st.write('You selected: ', option)
 
 # multi selector
 options = st.multiselect(
- 'What are your favorite clubs?', df['Club'].unique())
+ 'What are the most promising counties?', df['County'].unique())
 st.write('You selected:', options)
+
+#filter dataframe
+counties = st.multiselect('Show land under irrigation for County?', df['County'].unique())
+# Filter dataframe
+filtered_df = df[(df['County'].isin(counties)) ]
+# write dataframe to screen
+st.write(filtered_df)
+# plot data
+# create figure
+fig = plt.scatter(filtered_df, y='Percent')
+# Plot!
+st.plotly_chart(fig)
