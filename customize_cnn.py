9ebd882de0b0032b721e7f120d8a2c79db61e7d3
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-09 20:30:17 +0300

    Building a customizable CNN using streamlit

---
+++ customize_cnn.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import pydot_ng as pydot
+import streamlit as st
--- sample hello.py
+++ sample hello.py
@@ -3,6 +3,15 @@ import math
 import pandas as pd
 from matplotlib import pyplot as plt
 
+
+
+#df = st.cache(pd.read_csv)("football_data.csv")
+# @st.cache
+# def complex_func(a,b):
+#     DO SOMETHING COMPLEX
+# # Won't run again and again.
+# complex_func(a,b)
+st.header("Hello World Streamlit")
 x = st.slider('x')
 st.write(x, 'factorial is', math.factorial(x))
 # exploring widgets
@@ -38,9 +47,11 @@ filtered_df = df[(df['County'].isin(counties)) ]
 st.write(filtered_df)
 # plot data
 # create figure
-fig = plt.scatter(filtered_df, y='Percent')
+
+fig = plt.scatter(data=filtered_df, x='County', y='Percent')
 # Plot!
+#https://towardsdatascience.com/building-machine-learning-apps-with-streamlit-667cef3ff509
 '''
 ### Visualization of selected Counties
 '''
-st.plotly_chart(fig)
+st.pyplot(fig)
