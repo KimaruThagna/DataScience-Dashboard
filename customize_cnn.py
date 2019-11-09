96e671c615777b2af5c0a84b7e13c8a429e3250d
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-09 20:39:36 +0300

    customizable CNN via web

--- customize_cnn.py
+++ customize_cnn.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pydot_ng as pydot
 import streamlit as st
+from keras.datasets import fashion_mnist
+st.title('Customizable Convolutional Neural Network')
+st.header('Dataset: Fashion MNIST')
+st.text("This page allows you to build and train a CNN for the fashion MNIST dataset via an interactive visual interface")
