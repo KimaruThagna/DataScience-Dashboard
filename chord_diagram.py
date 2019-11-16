6b8552eba816287b94ca074d0b9deb5603f91e88
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-16 12:34:28 +0300

    define ideogram arc length using row sums

--- chord_diagram.py
+++ chord_diagram.py
@@ -1,14 +1,43 @@
 import plotly.plotly as py
 import plotly.figure_factory as ff
 import plotly.graph_objs as go
+import numpy as np
 # imaginary Sales data. 
 
-data = [['A', 'B', 'C', 'D', 'E', 'total'],
-        [16, 3, 28, 0, 18, 65],
-        [18, 0, 12, 5, 29, 64],
-        [9, 11, 17, 27, 0, 64],
-        [19, 0, 31, 11, 12, 73],
-        [23, 17, 10, 0, 34, 84]]
+data = [['A', 'B', 'C', 'D', 'E'],
+        [16, 3, 28, 0, 18],
+        [18, 0, 12, 5, 29],
+        [9, 11, 17, 27, 0],
+        [19, 0, 31, 11, 12],
+        [23, 17, 10, 0, 34]]
 
+
+matrix=np.array([[16,  3, 28,  0, 18],
+                 [18,  0, 12,  5, 29],
+                 [ 9, 11, 17, 27,  0],
+                 [19,  0, 31, 11, 12],
+                 [23, 17, 10,  0, 34]], dtype=int)
+L,M= matrix.shape
 table = ff.create_table(data, index=True)
-table.write_html('Data-Table.html', auto_open=True)
+#table.write_html('Data-Table.html', auto_open=True)
+
+'''
+Create ideograms which are arcs defined by the column total
+'''
+PI=np.pi
+
+def moduloAB(x, a, b): #maps a real number onto the unit circle identified with
+                       #the interval [a,b), b-a=2*PI
+        if a>=b:
+            raise ValueError('Incorrect interval ends')
+        y=(x-a)%(b-a)
+        return y+b if y<0 else y+a
+
+def test_2PI(x):
+    return 0<= x <2*PI
+
+row_sum=[np.sum(matrix[k,:]) for k in range(L)]
+
+#set the gap between two consecutive ideograms
+gap=2*PI*0.005
+ideogram_length=2*PI*np.asarray(row_sum)/sum(row_sum)-gap*np.ones(L) # length of the arcs based on row sum
