afa7538efd0258987943fa93520964c466d2b0f7
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-16 12:11:51 +0300

    display data table

--- chord_diagram.py
+++ chord_diagram.py
@@ -1,3 +1,14 @@
 import plotly.plotly as py
 import plotly.figure_factory as ff
 import plotly.graph_objs as go
+# imaginary Sales data. 
+
+data = [['A', 'B', 'C', 'D', 'E', 'total'],
+        [16, 3, 28, 0, 18, 65],
+        [18, 0, 12, 5, 29, 64],
+        [9, 11, 17, 27, 0, 64],
+        [19, 0, 31, 11, 12, 73],
+        [23, 17, 10, 0, 34, 84]]
+
+table = ff.create_table(data, index=True)
+table.write_html('Data-Table.html', auto_open=True)
