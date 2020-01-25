2e42ba04038ccfcc13346ab5fbb7434a7fe846e0
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-25 11:01:16 +0300

    load dataset

--- celluloid_gif.py
+++ celluloid_gif.py
@@ -1,3 +1,8 @@
 import matplotlib
+import pandas as pd
 from matplotlib import pyplot as plt
 from celluloid import Camera
+
+#load irrigation dataset
+irrigated_land = pd.read_csv('datasets/land_under_Irrigation.csv')
+print(irrigated_land.head())
